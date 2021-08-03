1002578f5f5888e7ff8e4a21fd30797474879a31
Diego448 <diegosg448@gmail.com>
2021-08-02 20:58:38 -0500

    Adding test example

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello there!"
+
+@app.route('/test')
+def test():
+    return render_template('homepage.html')
