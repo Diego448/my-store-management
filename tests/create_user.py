4c80b4530a9eb2ea26e44acc7867cb96deaf2376
Diego448 <diegosg448@gmail.com>
2021-09-02 22:05:53 -0500

    Updating create_user script to accept user input

--- tests/create_user.py
+++ tests/create_user.py
@@ -1,4 +1,8 @@
-import requests
+import requests, json
 
-r = requests.post('http://localhost:5000/users')
-print(r.text)
+headers = {'content-type': 'application/json'}
+name = input('Enter name: ')
+password = input('Enter password: ')
+data = json.dumps({"name":name, "password": password})
+response = requests.post("http://127.0.0.1:5000/user/create", data = data, headers=headers)
+print(response.text)
