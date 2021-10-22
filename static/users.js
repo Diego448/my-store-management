f722a7a3fdf863247ccce98a6583e04164302ea3
Diego448 <diegosg448@gmail.com>
2021-10-21 20:40:23 -0500

    Fixing behaviour of toggle edit/update modal

--- static/users.js
+++ static/users.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+    document.getElementById("modalClose").addEventListener("click", (event) => {
+        resetForm();
+    });
+}
+
 async function getUserDetails(id) {
     return fetch("/user/" + id).then(
         response => response.json()
@@ -8,14 +14,21 @@ async function getUserDetails(id) {
 
 async function fillUserEditModal(id) {
     let nameInput = document.getElementById("name");
+    let userInput = document.getElementById("username");
+    let emailInput = document.getElementById("email");
+    let modalEdit = document.getElementById("modalEdit");
     let modalTitle = document.getElementById("exampleModalLabel");
     let data = await getUserDetails(id);
+    modalEdit.removeAttribute("hidden");
     modalTitle.innerHTML = "Editar usuario";
     nameInput.value = data["name"];
+    emailInput.value = data["email"];
+    userInput.value = data["username"];
 }
 
 async function addUser() {
     let modalTitle = document.getElementById("exampleModalLabel");
+    let modalSave = document.getElementById("modalSave");
     let nameInput = document.getElementById("name");
     let userInput = document.getElementById("username");
     let emailInput = document.getElementById("email");
@@ -23,6 +36,7 @@ async function addUser() {
     let privilegesInput = document.getElementById("privileges");
 
     modalTitle.innerHTML = "Agregar usuario";
+    modalSave.removeAttribute("hidden");
 
     let userData = {
         "name": nameInput.value,
@@ -42,21 +56,35 @@ async function addUser() {
 
       if (response.ok) {
           document.getElementById("modalClose").click();
+          resetForm();
       }
-
+    
     return response.json();
+}
+
+async function updateUser(id) {
+    resetForm();
 }
 
 function resetForm() {
     let modalTitle = document.getElementById("exampleModalLabel");
     let modalForm = document.getElementById("modalForm");
+    let modalSave = document.getElementById("modalSave");
+    let modalEdit = document.getElementById("modalEdit");
     let nameInput = document.getElementById("name");
     let userInput = document.getElementById("username");
     let emailInput = document.getElementById("email");
     let passwordInput = document.getElementById("password");
     let privilegesInput = document.getElementById("privileges");
 
-    modalTitle.innerHTML("Agregar usuario");
-    modalForm.resetForm();
+    modalTitle.innerHTML = "Agregar usuario";
+    modalSave.setAttribute("hidden", "true");
+    modalEdit.setAttribute("hidden", "true");
+    modalForm.reset();
 
+}
+
+function toggleAdd() {
+    resetForm();
+    document.getElementById("modalSave").removeAttribute("hidden");
 }
