140199a6c38bb67f3dcb27471181012ae7c49f2b
Diego448 <diegosg448@gmail.com>
2021-10-21 19:53:57 -0500

    Changing frontend requests from python approach to JS

--- static/users.js
+++ static/users.js
@@ -13,3 +13,50 @@ async function fillUserEditModal(id) {
     modalTitle.innerHTML = "Editar usuario";
     nameInput.value = data["name"];
 }
+
+async function addUser() {
+    let modalTitle = document.getElementById("exampleModalLabel");
+    let nameInput = document.getElementById("name");
+    let userInput = document.getElementById("username");
+    let emailInput = document.getElementById("email");
+    let passwordInput = document.getElementById("password");
+    let privilegesInput = document.getElementById("privileges");
+
+    modalTitle.innerHTML = "Agregar usuario";
+
+    let userData = {
+        "name": nameInput.value,
+        "username": userInput.value,
+        "email": emailInput.value,
+        "password": passwordInput.value,
+        "privileges": privilegesInput.value
+    }
+
+    const response = await fetch("/user/create", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      });
+
+      if (response.ok) {
+          document.getElementById("modalClose").click();
+      }
+
+    return response.json();
+}
+
+function resetForm() {
+    let modalTitle = document.getElementById("exampleModalLabel");
+    let modalForm = document.getElementById("modalForm");
+    let nameInput = document.getElementById("name");
+    let userInput = document.getElementById("username");
+    let emailInput = document.getElementById("email");
+    let passwordInput = document.getElementById("password");
+    let privilegesInput = document.getElementById("privileges");
+
+    modalTitle.innerHTML("Agregar usuario");
+    modalForm.resetForm();
+
+}
