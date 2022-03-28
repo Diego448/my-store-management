4c73c031bceff745398d9e6fe6ec8ae7509d84ad
Diego448 <diegosg448@gmail.com>
2022-03-27 18:40:46 -0600

    Updating logic and adding models support

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import re
 from fastapi import FastAPI, Request
 import db_utils
+from models import User
 
 app = FastAPI()
 
@@ -12,13 +13,13 @@ def home():
 async def login():
     return {'login_status': 'success'}
 
-@app.get('/users')
+@app.get('/user/all')
 async def get_users():
     return db_utils.get_users()
 
 @app.post('/user/create')
-async def create_user(request: Request):
-    data_body = await request.json()
+async def create_user(request: Request, user: User):
+    data_body = user.dict()
     return db_utils.add_user(data_body)
 
 @app.get('/user/{user_id}')
