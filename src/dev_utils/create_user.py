807164faaddc286609cee7aafef6c1fa58544fc7
Diego448 <diegosg448@gmail.com>
2022-03-27 18:24:28 -0600

    Removing unnecesary files

---
+++ src/dev_utils/create_user.py
@@ -0,0 +1,8 @@
+import requests, json
+
+headers = {'content-type': 'application/json'}
+name = input('Enter name: ')
+password = input('Enter password: ')
+data = json.dumps({"name":name, "password": password})
+response = requests.post("http://127.0.0.1:8000/user/create", data = data, headers=headers)
+print(response.text)
--- tests/create_user.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-headers = {'content-type': 'application/json'}
-name = input('Enter name: ')
-password = input('Enter password: ')
-data = json.dumps({"name":name, "password": password})
-response = requests.post("http://127.0.0.1:5000/user/create", data = data, headers=headers)
-print(response.text)
